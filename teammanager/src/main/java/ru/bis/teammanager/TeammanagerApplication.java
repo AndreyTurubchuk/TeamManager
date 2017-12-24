04e6373048bfedb78c0992acb81cd379a6519d23
Andrey Turubchuk <antur1977@rambler.ru>
2017-12-24 13:15:53 +0300

    create project

---
+++ teammanager/src/main/java/ru/bis/teammanager/TeammanagerApplication.java
@@ -0,0 +1,12 @@
+package ru.bis.teammanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammanagerApplication.class, args);
+	}
+}
