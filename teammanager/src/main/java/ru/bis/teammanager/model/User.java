c9cdae16837de71f25b90ab4039005cc31b32f86
Andrey Turubchuk <antur1977@rambler.ru>
2017-12-24 15:08:40 +0300

    add entity user, create user controller

---
+++ teammanager/src/main/java/ru/bis/teammanager/model/User.java
@@ -0,0 +1,56 @@
+package ru.bis.teammanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+    private String name;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
