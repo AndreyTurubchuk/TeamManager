125df8537d87eb1bee1fa58dd0d5cc9124786337
Andrey Turubchuk <antur1977@rambler.ru>
2017-12-27 01:49:20 +0300

    add view and controller delete

--- teammanager/src/main/java/ru/bis/teammanager/service/PersonService.java
+++ teammanager/src/main/java/ru/bis/teammanager/service/PersonService.java
@@ -6,5 +6,9 @@ import java.util.List;
 
 public interface PersonService {
    List<Person> getAll();
+   Person getOne(long id);
    void savePerson(Person person);
+   void saveAndFlushPerson(Person person);
+   void flushPerson();
+   void delete(long id);
    }
