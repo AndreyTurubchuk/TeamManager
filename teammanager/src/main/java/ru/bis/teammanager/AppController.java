e9e7ee21923396f2e6903ad71c3fe044f13dfb95
Andrey Turubchuk <antur1977@rambler.ru>
2018-01-14 16:01:40 +0300

    create mail service

--- teammanager/src/main/java/ru/bis/teammanager/AppController.java
+++ teammanager/src/main/java/ru/bis/teammanager/AppController.java
@@ -8,32 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.bis.teammanager.model.Letter;
 import ru.bis.teammanager.model.Person;
-import ru.bis.teammanager.repositories.PersonRepository;
 import ru.bis.teammanager.service.LetterService;
 import ru.bis.teammanager.service.NotificationService;
 import ru.bis.teammanager.service.PersonService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Controller
 @RequestMapping(value = "/person")
-//@RequestMapping(value = "/teammanager/person")
 public class AppController {
-
-    private final static String MESSAGE_HIRED = "You are hired";
-    private final static String MESSAGE_SUBJECT = "Message";
 
     @Autowired
     private PersonService personService;
 
     @Autowired
     private LetterService letterService;
-
-
-    @Autowired
-    private NotificationService notificationService;
 
     @RequestMapping({"/list"})
     public String personList(Model model) {
@@ -42,9 +32,9 @@ public class AppController {
         return "personList";
     }
 
-    @RequestMapping(value = "/delete/{personId}", method = RequestMethod.GET)
-    public String delete(@PathVariable long personId) {
-        personService.delete(personId);
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable long id) {
+        personService.delete(id);
         return "redirect:/person/list";
     }
 
@@ -54,70 +44,37 @@ public class AppController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String add2(Person person) {
+    public String add(Person person) {
         personService.savePerson(person);
-        Letter letter = new Letter("sokoldemian1990@mail.ru", person.getEmailAddress(), MESSAGE_SUBJECT, MESSAGE_HIRED, person);
-     //   letterService.saveLetter(letter);
-        try {
-            notificationService.sendNotification(person, letter);
-        } catch (MailException e) {
-            e.printStackTrace();
-        }
         return "redirect:/person/list";
     }
 
     @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
-    public String view(@PathVariable Long id, Model model5) {
+    public String view(@PathVariable Long id, Model model) {
         Person person = personService.getOne(id);
         List<Letter> letterList = letterService.findByTo(person.getEmailAddress());
-        model5.addAttribute("personLetterList", letterList);
-        model5.addAttribute("personFullName", person.getFirstName() + " " + person.getLastName());
+        model.addAttribute("personLetterList", letterList);
+        model.addAttribute("personFullName", person.getFirstName() + " " + person.getLastName());
         return "personLetterList";
     }
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public String edit(@PathVariable Long id, Model model4) {
+    public String edit(@PathVariable Long id, Model model) {
         Person person = personService.getOne(id);
-        model4.addAttribute("person", person);
+        model.addAttribute("person", person);
         return "personEdit";
     }
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
-    public String edit(Person person) {
-        personService.savePerson(person);
+    public String edit(@PathVariable Long id, Person personDetails) {
+        personService.editPerson(id, personDetails);
         return "redirect:/person/list";
     }
 
     @RequestMapping(value = "/")
-    public String createPerson(Model model2) {
-   //     Person person = new Person("Andrey", "Turubchuk", "antur1977@mail.ru");
-   //     Person person2 = new Person("Andrey2", "Turubchuk2", "an2");
-     //   personService.savePerson(person);
-    //    personService.savePerson(person2);
-     //   Letter letter = new Letter("antur1977@mail.ru", "antur1977@mail.ru", "Main Message", "This is s BIS", person);
-/*        try {
-            notificationService.sendNotification(person2, letter);
-        } catch (MailException e) {
-            e.printStackTrace();
-        }*/
-     //   List<Person> personList = personService.getAll();
-     //   model2.addAttribute("personList", personList);
+    public String main() {
         return "personList";
     }
-
-    @RequestMapping(value = "/createmessage")
-    public String createMessage(Model model3) {
-        Person person = new Person("Andrey", "Turubchuk", "antur1977@mail.ru");
-        personService.savePerson(person);
-        Letter letter2 = new Letter("antur1977@mail.ru", "antur1977@mail.ru", "Main Message", "This is s BIS", person);
-        letterService.saveLetter(letter2);
-        List<Letter> letterList2 = letterService.findByTo("antur1977@mail.ru");
-        model3.addAttribute("personLetterList", letterList2);
-       return "personLetterList";
-    }
-
-
-
 }
 
 
