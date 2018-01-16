d30f9588154031b3986c576f3b6fda18f16ae032
Andrey Turubchuk <antur1977@rambler.ru>
2018-01-16 23:59:55 +0300

    add error.html

--- teammanager/src/main/java/ru/bis/teammanager/service/NotificationService.java
+++ teammanager/src/main/java/ru/bis/teammanager/service/NotificationService.java
@@ -1,8 +1,9 @@
 package ru.bis.teammanager.service;
 
+import org.springframework.mail.MailException;
 import ru.bis.teammanager.model.Letter;
 import ru.bis.teammanager.model.Person;
 
 public interface NotificationService {
-    void sendNotification(Person person, Letter letter);
+    void sendNotification(Person person, Letter letter) throws MailException;
 }
